cc772c169e6217fbcbf302775465fc4b3060e6d9
Ganesh.More <ganesh.more@contentserv.com>
2021-07-01 09:11:58 +0530

    count sent to the database

--- src/counter.js
+++ src/counter.js
@@ -1,47 +1,65 @@
 import './App.css';
-
+import axios from 'axios';
 import React, { useState} from 'react';
 
+let timer = null;
 
 function Counter({maxCount = 1000, minCount = 1}) {
   const [count, setCount] = useState(minCount);
 
-  const handleCountChanged = (oEvent) => {
+
+  const handleOnChanged = (oEvent) => {
     let sValue = oEvent.target.value;
     if (sValue >= minCount && sValue <= maxCount) {
       setCount(sValue * 1);
+      handleCountChanged(sValue * 1);
     }
     else {
       setCount(count);
+      handleCountChanged(count);
     }
   };
 
+  const handleCountChanged = (newCount) => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(updateCountInDataBase.bind(this, newCount), 1000);
+  };
+
+  let updateCountInDataBase  = async(newCount) => {
+    clearTimeout(timer);
+    let oRequestData = {counter: newCount};
+    await axios.post('https://interview-8e4c5-default-rtdb.firebaseio.com/front-end.json', oRequestData);
+  };
+
+
   const incrementCount = () => {
     setCount((prevCount) => {
+      let newCount = prevCount;
       if (prevCount < maxCount) {
-        return prevCount + 1;
+        newCount = prevCount + 1;
       }
-      else {
-        return prevCount;
-      }
+      handleCountChanged(newCount);
+      return newCount
     });
   };
 
   const decreaseCount = () => {
     setCount((prevCount) => {
+      let newCount = prevCount;
       if (prevCount > minCount) {
-        return prevCount - 1;
+        newCount = prevCount - 1;
       }
-      else {
-        return prevCount;
-      }
+      handleCountChanged(newCount);
+      return newCount;
     });
   };
   return (
       <div className={"artBoard"}>
         <div className={"rectangle"}>
           <div className={"minus"} onClick={decreaseCount}> - </div>
-          <input type={"text"} className={"count"} value={count} onChange={handleCountChanged}/>
+          <input type={"text"} className={"count"} value={count} onChange={handleOnChanged}/>
           <div className={"plus"} onClick={incrementCount}> + </div>
         </div>
       </div>
